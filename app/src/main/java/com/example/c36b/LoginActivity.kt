ae571ad4806c0ce69735605d66fc7715c71ce5b9
sprajapati123 <sandisprajapati52@gmail.com>
2025-04-27 09:04:45 +0545

    commitd

--- app/src/main/java/com/example/c36b/LoginActivity.kt
+++ app/src/main/java/com/example/c36b/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.c36b
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,13 +19,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +45,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,6 +70,8 @@ class LoginActivity : ComponentActivity() {
 
 @Composable
 fun LoginBody(innerPadding: PaddingValues) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .padding(innerPadding)
@@ -108,8 +124,10 @@ fun LoginBody(innerPadding: PaddingValues) {
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .height(100.dp)
-                    .width(100.dp).clip(
-                        shape = RoundedCornerShape(100.dp))
+                    .width(100.dp)
+                    .clip(
+                        shape = RoundedCornerShape(100.dp)
+                    )
             )
             Column(
                 verticalArrangement = Arrangement.Center,
@@ -118,7 +136,7 @@ fun LoginBody(innerPadding: PaddingValues) {
                 Text(text = "174")
                 Text(text = "Posts")
             }
-            Column (
+            Column(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -128,11 +146,17 @@ fun LoginBody(innerPadding: PaddingValues) {
             Column(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
-            )  {
+            ) {
                 Text(text = "600k")
                 Text(text = "Followings")
             }
         }
+
+        Spacer(modifier = Modifier.padding(20.dp))
+
+
+
+
     }
 }
 
