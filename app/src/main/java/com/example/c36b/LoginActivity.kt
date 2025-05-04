1bdee5e89cb84692a20afbc594486b600dc26bab
sprajapati123 <sandisprajapati52@gmail.com>
2025-05-04 09:56:42 +0545

    login acitivity

--- app/src/main/java/com/example/c36b/LoginActivity.kt
+++ app/src/main/java/com/example/c36b/LoginActivity.kt
@@ -1,267 +1,54 @@
 package com.example.c36b
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.c36b.ui.theme.C36BTheme
-import com.example.c36b.ui.theme.Green40
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
             Scaffold { innerPadding ->
                 LoginBody(innerPadding)
             }
         }
     }
-
-
 }
 
 @Composable
 fun LoginBody(innerPadding: PaddingValues) {
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .padding(innerPadding)
             .fillMaxSize()
-            .background(color = Color.White)
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            //method 1
-            Icon(
-                imageVector = Icons.Default.KeyboardArrowLeft,
-                contentDescription = null,
-//            tint = Color.Green,
-                modifier = Modifier
-                    .height(30.dp)
-                    .width(30.dp)
-            )
-            Text(
-                text = "Andrew",
-                style = TextStyle(
-                    fontSize = 15.sp,
-                    fontWeight = FontWeight.Bold,
-//                    fontStyle = FontStyle.Italic,
-//                    textDecoration = TextDecoration.Underline
-                )
-            )
-            //method 2
-            Icon(
-                painter = painterResource(R.drawable.baseline_more_horiz_24),
-                contentDescription = null,
-//            tint = Color.Green,
-                modifier = Modifier
-                    .height(30.dp)
-                    .width(30.dp)
-            )
-        }
-
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            Image(
-                painter = painterResource(R.drawable.person),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .height(100.dp)
-                    .width(100.dp)
-                    .clip(
-                        shape = RoundedCornerShape(100.dp)
-                    )
-            )
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "174")
-                Text(text = "Posts")
-            }
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "772k")
-                Text(text = "Followers")
-            }
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "600k")
-                Text(text = "Followings")
-            }
-        }
-
-        Column(
-            modifier =
-            Modifier.padding(
-                horizontal = 10.dp,
-                vertical = 15.dp
-            )
-        ) {
-            Text(
-                "Andrew Queo", style = TextStyle(
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Red,
-                    fontSize = 19.sp
-                )
-            )
-            Text("Artist", modifier = Modifier.alpha(0.5f))
-            Text("DESIGNER")
-            Text("isabella@art.design")
-            Text("Followed by jeena and anna")
-
-
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            Button(
-                onClick = {
-                    // user le button thiche pachi ke garne
-                    //                to-do when clicked
-                },
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Blue,
-                    contentColor = Color.White
-                )
-            ) {
-                Text("Follow")
-            }
-
-            OutlinedButton(
-                onClick = {
-                    // user le button thiche pachi ke garne
-//                to-do when clicked
-                },
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    contentColor = Color.Black
-                )
-            ) {
-                Text("Message")
-            }
-
-            OutlinedButton(
-                onClick = {
-                    // user le button thiche pachi ke garne
-//                to-do when clicked
-                },
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    contentColor = Color.Black
-                )
-            ) {
-                Text("Email")
-            }
-
-            OutlinedButton(
-                onClick = {
-                    // user le button thiche pachi ke garne
-//                to-do when clicked
-                },
-                shape = RoundedCornerShape(10.dp),
-//                border = BorderStroke(1.dp,Color.Red),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    contentColor = Color.Black
-                )
-            ) {
-                Icon(
-                    imageVector = Icons.Default.KeyboardArrowDown,
-                    contentDescription = null
-                )
-            }
-        }
-
-        Spacer(modifier = Modifier.height(50.dp))
-        Row {
-            Card(
-                modifier = Modifier.height(300.dp).weight(1f)
-            ) {
-
-            }
-            Spacer(modifier = Modifier.width(50.dp))
-            Card(
-                modifier = Modifier.height(300.dp).weight(3f),
-                colors = CardDefaults.cardColors(
-                    containerColor = Green40
-                )
-            ) {
-
-            }
-        }
-
+        Image(
+            painter = painterResource(R.drawable.img),
+            contentDescription = null,
+            modifier = Modifier.height(250.dp).width(250.dp)
+        )
 
     }
 }
@@ -269,7 +56,6 @@ fun LoginBody(innerPadding: PaddingValues) {
 
 @Preview
 @Composable
-fun PreviewBody() {
+fun LoginPreviewBody() {
     LoginBody(innerPadding = PaddingValues(0.dp))
 }
-
