3539986adb6d8c467359f50827f118a7ffc92523
sprajapati123 <sandisprajapati52@gmail.com>
2025-05-25 10:30:57 +0545

    scroll

--- app/src/main/java/com/example/c36b/ListViewActivity.kt
+++ app/src/main/java/com/example/c36b/ListViewActivity.kt
@@ -5,19 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,12 +42,65 @@ class ListViewActivity : ComponentActivity() {
 fun ListBody() {
     val scrollState = rememberScrollState()
 
+    val scrollState2 = rememberScrollState()
+
+
+
     Scaffold { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize().verticalScroll(scrollState)
+                .fillMaxSize()
+                .verticalScroll(scrollState)
         ) {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .horizontalScroll(scrollState2)
+
+            ) {
+                Box(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                        .background(color = Color.Red)
+                )
+                Box(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                        .background(color = Color.Magenta)
+                )
+                Box(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                        .background(color = Color.Yellow)
+                )
+                Box(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                        .background(color = Color.Green)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                        .background(color = Color.DarkGray)
+                )
+                Box(
+                    modifier = Modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                        .background(color = Color.Cyan)
+                )
+
+
+            }
+
             Box(
                 modifier = Modifier
                     .height(300.dp)
@@ -58,6 +116,8 @@ fun ListBody() {
                     .padding(20.dp)
                     .background(color = Color.Gray)
             )
+
+
 
             Box(
                 modifier = Modifier
