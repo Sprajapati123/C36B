00c540515cbe70f20b8254a2cd251926593a0b28
sprajapati123 <sandisprajapati52@gmail.com>
2025-05-26 10:08:43 +0545

    lazyrow

--- app/src/main/java/com/example/c36b/ListViewActivity.kt
+++ app/src/main/java/com/example/c36b/ListViewActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -28,6 +30,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.c36b.ui.theme.C36BTheme
@@ -45,109 +49,148 @@ class ListViewActivity : ComponentActivity() {
 @Composable
 fun ListBody() {
 
+    val images = listOf(
+        R.drawable.person,
+        R.drawable.img,
+        R.drawable.img_1,
+        R.drawable.img_2,
+        R.drawable.img_3,
+        R.drawable.img_4,
+        R.drawable.img_5,
+        R.drawable.img_6,
+    )
 
-
+    val name = listOf<String>(
+        "Ram",
+        "Info",
+        "Momo",
+        "Pizza",
+        "Panipuri",
+        "Grass",
+        "Samosa",
+        "chatpate",
+    )
 
 
     Scaffold { innerPadding ->
-        LazyColumn (
+        LazyColumn(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
             item {
-            LazyRow (
-                modifier = Modifier
-                    .fillMaxWidth()
 
-            ) {
-                item {
+                LazyRow(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    items(images.size) { index ->
+                        Image(
+                            painterResource(images[index]),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(100.dp)
+                                .border( width = 1.dp,color = Color.Blue,
+                                    shape = CircleShape
+                                )
+                                .clip(shape = CircleShape)
+                        )
 
+                    }
+                }
+
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+
+                ) {
+                    item {
+
+
+                        Box(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(100.dp)
+                                .background(color = Color.Red)
+                        )
+                        Box(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(100.dp)
+                                .background(color = Color.Magenta)
+                        )
+                        Box(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(100.dp)
+                                .background(color = Color.Yellow)
+                        )
+                        Box(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(100.dp)
+                                .background(color = Color.Green)
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(100.dp)
+                                .background(color = Color.DarkGray)
+                        )
+                        Box(
+                            modifier = Modifier
+                                .height(100.dp)
+                                .width(100.dp)
+                                .background(color = Color.Cyan)
+                        )
+
+
+                    }
+                }
 
                 Box(
                     modifier = Modifier
-                        .height(100.dp)
-                        .width(100.dp)
-                        .background(color = Color.Red)
+                        .height(300.dp)
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                        .background(color = Color.Black)
                 )
+
                 Box(
                     modifier = Modifier
-                        .height(100.dp)
-                        .width(100.dp)
+                        .height(300.dp)
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                        .background(color = Color.Gray)
+                )
+
+
+
+                Box(
+                    modifier = Modifier
+                        .height(300.dp)
+                        .fillMaxWidth()
+                        .padding(20.dp)
                         .background(color = Color.Magenta)
                 )
+
                 Box(
                     modifier = Modifier
-                        .height(100.dp)
-                        .width(100.dp)
-                        .background(color = Color.Yellow)
-                )
-                Box(
-                    modifier = Modifier
-                        .height(100.dp)
-                        .width(100.dp)
+                        .height(300.dp)
+                        .fillMaxWidth()
+                        .padding(20.dp)
                         .background(color = Color.Green)
                 )
 
                 Box(
                     modifier = Modifier
-                        .height(100.dp)
-                        .width(100.dp)
-                        .background(color = Color.DarkGray)
+                        .height(300.dp)
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                        .background(color = Color.Yellow)
                 )
-                Box(
-                    modifier = Modifier
-                        .height(100.dp)
-                        .width(100.dp)
-                        .background(color = Color.Cyan)
-                )
-
-
             }
-            }
-
-            Box(
-                modifier = Modifier
-                    .height(300.dp)
-                    .fillMaxWidth()
-                    .padding(20.dp)
-                    .background(color = Color.Black)
-            )
-
-            Box(
-                modifier = Modifier
-                    .height(300.dp)
-                    .fillMaxWidth()
-                    .padding(20.dp)
-                    .background(color = Color.Gray)
-            )
-
-
-
-            Box(
-                modifier = Modifier
-                    .height(300.dp)
-                    .fillMaxWidth()
-                    .padding(20.dp)
-                    .background(color = Color.Magenta)
-            )
-
-            Box(
-                modifier = Modifier
-                    .height(300.dp)
-                    .fillMaxWidth()
-                    .padding(20.dp)
-                    .background(color = Color.Green)
-            )
-
-            Box(
-                modifier = Modifier
-                    .height(300.dp)
-                    .fillMaxWidth()
-                    .padding(20.dp)
-                    .background(color = Color.Yellow)
-            )
-        }
         }
     }
 }
