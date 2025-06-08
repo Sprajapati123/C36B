1d554fa15a0fa3f3074683c28a12da8912cae5c7
sprajapati123 <sandisprajapati52@gmail.com>
2025-06-08 09:16:06 +0545

    add product

---
+++ app/src/main/java/com/example/c36b/view/AddProductActivity.kt
@@ -0,0 +1,88 @@
+package com.example.c36b.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.c36b.view.ui.theme.C36BTheme
+
+class AddProductActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AddProductBody()
+        }
+    }
+}
+
+@Composable
+fun AddProductBody() {
+    var pName by remember { mutableStateOf("") }
+    var pPrice by remember { mutableStateOf("") }
+    var pDesc by remember { mutableStateOf("") }
+
+    Scaffold { innerPadding ->
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)) {
+            item {
+                OutlinedTextField(
+                    value = pName,
+                    onValueChange = {
+                        pName = it
+                    },
+                    placeholder = {
+                        Text("Product Name")
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = pDesc,
+                    onValueChange = {
+                        pDesc = it
+                    },
+                    placeholder = {
+                        Text("Product Description")
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = pPrice,
+                    onValueChange = {
+                        pPrice = it
+                    },
+                    placeholder = {
+                        Text("Product Price")
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Button(onClick = {}) {
+                    Text("Add Product")
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AddProductPreview() {
+    AddProductBody()
+}
