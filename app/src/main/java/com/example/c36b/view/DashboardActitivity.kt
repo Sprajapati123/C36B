50a2b09565540f20050513238c0182e3f18c1768
sprajapati123 <sandisprajapati52@gmail.com>
2025-06-09 10:15:06 +0545

    dashboard activity

--- app/src/main/java/com/example/c36b/view/DashboardActitivity.kt
+++ app/src/main/java/com/example/c36b/view/DashboardActitivity.kt
@@ -8,17 +8,26 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.c36b.repository.ProductRepository
+import com.example.c36b.repository.ProductRepositoryImpl
+import com.example.c36b.viewmodel.ProductViewModel
 
 class DashboardActitivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +40,19 @@ class DashboardActitivity : ComponentActivity() {
 }
 
 @Composable
-fun DashboardBody(){
+fun DashboardBody() {
     val context = LocalContext.current
     val activity = context as? Activity
+
+    val repo = remember { ProductRepositoryImpl() }
+    val viewModel = remember { ProductViewModel(repo) }
+
+    val products = viewModel.allProducts.
+                    observeAsState(initial = emptyList())
+
+    LaunchedEffect(Unit) {
+        viewModel.getAllProduct()
+    }
 
     Scaffold(
         floatingActionButton = {
@@ -41,21 +60,34 @@ fun DashboardBody(){
                 val intent = Intent(context, AddProductActivity::class.java)
                 context.startActivity(intent)
             }) {
-                Icon(Icons.Default.Add,contentDescription = null)
+                Icon(Icons.Default.Add, contentDescription = null)
             }
         },
 
-    ) { innerPadding->
-        Column (
-            modifier = Modifier.padding(innerPadding).fillMaxSize()
-        ){
-
+        ) { innerPadding ->
+        LazyColumn (
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            items(products.value.size) { index ->
+                val eachProduct = products.value[index]
+                Card (
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    Column {
+                        Text("${eachProduct?.productName}")
+                        Text("${eachProduct?.productDesc}")
+                        Text("${eachProduct?.productDesc}")
+                    }
+                }
+            }
         }
     }
 }
 
 @Preview
 @Composable
-fun previewDashboard(){
+fun previewDashboard() {
     DashboardBody()
 }
