4ec23688ec7eee6bce7521b00201ba6da3eaca9b
sprajapati123 <sandisprajapati52@gmail.com>
2025-06-11 10:50:33 +0545

    changes

--- app/src/main/java/com/example/c36b/view/DashboardActitivity.kt
+++ app/src/main/java/com/example/c36b/view/DashboardActitivity.kt
@@ -6,16 +6,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,8 +30,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.c36b.repository.ProductRepository
 import com.example.c36b.repository.ProductRepositoryImpl
 import com.example.c36b.viewmodel.ProductViewModel
@@ -47,12 +56,12 @@ fun DashboardBody() {
     val repo = remember { ProductRepositoryImpl() }
     val viewModel = remember { ProductViewModel(repo) }
 
-    val products = viewModel.allProducts.
-                    observeAsState(initial = emptyList())
+    val products = viewModel.allProducts.observeAsState(initial = emptyList())
 
     LaunchedEffect(Unit) {
         viewModel.getAllProduct()
     }
+
 
     Scaffold(
         floatingActionButton = {
@@ -65,23 +74,44 @@ fun DashboardBody() {
         },
 
         ) { innerPadding ->
-        LazyColumn (
+        LazyColumn(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
             items(products.value.size) { index ->
-                val eachProduct = products.value[index]
-                Card (
-                    modifier = Modifier.fillMaxWidth()
-                ){
-                    Column {
-                        Text("${eachProduct?.productName}")
-                        Text("${eachProduct?.productDesc}")
-                        Text("${eachProduct?.productDesc}")
+                val data = products.value[index]
+
+                Card(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)) {
+                    Column(modifier = Modifier.padding(12.dp)) {
+                        Text("${data?.productName}")
+                        Text("${data?.productPrice}")
+                        Text("${data?.productDesc}")
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.End
+                            ) {
+                            IconButton(
+                                onClick = {},
+                                colors = IconButtonDefaults.iconButtonColors(
+                                    contentColor = Color.Gray
+                                )
+                                ) {
+                                Icon(Icons.Default.Edit, contentDescription = null)
+                            }
+                            IconButton(onClick = {}, colors = IconButtonDefaults.iconButtonColors(
+                                contentColor = Color.Red
+                            )) {
+                                Icon(Icons.Default.Delete, contentDescription = null)
+                            }
+                        }
                     }
                 }
             }
+
         }
     }
 }
