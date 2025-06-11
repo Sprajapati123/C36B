d81157ff9668d05588ae72d575d2d2cad1a9db18
sprajapati123 <sandisprajapati52@gmail.com>
2025-06-11 11:43:19 +0545

    updat

--- app/src/main/java/com/example/c36b/view/UpdateProductActivity.kt
+++ app/src/main/java/com/example/c36b/view/UpdateProductActivity.kt
@@ -15,7 +15,9 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -49,6 +51,18 @@ fun UpdateProductBody() {
     var pName by remember { mutableStateOf("") }
     var pPrice by remember { mutableStateOf("") }
     var pDesc by remember { mutableStateOf("") }
+
+    val productId: String? = activity?.intent?.getStringExtra("productId")
+
+    val products = viewModel.products.observeAsState(initial = null)
+
+    LaunchedEffect(Unit) {
+        viewModel.getProductById(productId.toString())
+    }
+
+    pName = products.value?.productName ?: ""
+    pDesc = products.value?.productDesc ?: ""
+    pPrice = products.value?.productPrice.toString()
 
     Scaffold { innerPadding ->
         LazyColumn(
@@ -89,6 +103,21 @@ fun UpdateProductBody() {
                 )
 
                 Button(onClick = {
+                    var data = mutableMapOf<String, Any?>()
+
+                    data["productId"] = products.value?.productId
+                    data["productName"] = pName
+                    data["productDesc"] = pDesc
+                    data["productPrice"] = pPrice.toDouble()
+
+                    viewModel.updateProduct(products.value?.productId.toString(),data){
+                        success,message->
+                        if(success){
+                            activity?.finish()
+                        }else{
+                            Toast.makeText(context,message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
 
                 }) {
                     Text("Update Product")
@@ -100,6 +129,6 @@ fun UpdateProductBody() {
 
 @Preview
 @Composable
-fun preUpdate(){
+fun preUpdate() {
     UpdateProductBody()
 }
